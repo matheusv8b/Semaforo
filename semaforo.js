b7b053ce2a768996748d084dc93e63bb8fb7327a
Matheus de Jesus <matheusv0b@hotmail.com>
2022-12-05 10:39:48 -0300

    Update semaforo.js

--- semaforo.js
+++ semaforo.js
@@ -1,15 +1,39 @@
 const img = document.getElementById("img");
 const buttons = document.getElementById("buttons");
+let colorIndex = 0;
+let intervalId = null;
 
-const trafficLght = (event) => {
+const trafficLight = (event) => {
     turnOn[event.target.id]();
+    stopAutomatic();
+}
+
+const nextIndex = () => {
+    if (colorIndex < 2) {
+    colorIndex++
+    }else {
+        colorIndex = 0;
+    }
+}
+
+const changecolor = () => {
+    const colors = ['red','yellow','green']
+    const color = colors[colorIndex];
+    turnOn[color]();
+    colorIndex++;
+    nextIndex();
+}
+
+const stopAutomatic = () => {
+    clearInterval()
 }
 
 const turnOn = {
     'red': () => img.src = './semaforo-vermelho.jpg',
     'yellow': () => img.src = './semaforo-amarelo.jpg',
     'green': () => img.src = './semaforo-verde.jpg',
+    'automatic': () => intervalId = setInterval(changecolor, 1000)
 }
 
 
-buttons.addEventListener('click', trafficLght);
+buttons.addEventListener('click', trafficLight);
